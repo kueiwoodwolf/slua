298b6538c4b6154195371dee0f7ea010a6e65f55
hjs <kueiwoodwolf@qq.com>
2019-07-23 02:00:37 -0700

    rebuild_bytes_helper

--- build/make_bytes_helper.sh
+++ build/make_bytes_helper.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mcs -langversion:ISO-2 -sdk:2 /unsafe -target:library BytesHelper.cs
+mcs -langversion:ISO-2 -sdk:4.5 /unsafe -target:library BytesHelper.cs
 cp BytesHelper.dll ../Assets/Plugins/Slua_Managed/BytesHelper.dll
